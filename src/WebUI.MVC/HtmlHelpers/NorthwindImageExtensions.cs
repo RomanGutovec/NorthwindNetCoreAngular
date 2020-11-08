b4508768b7d1d52e6cbd43adfc86408532100a6e
Raman Hutavets <raman_hutavets@epam.com>
2020-11-08 13:46:36 +0300

    Task-3/Add tah and html helpers.

---
+++ src/WebUI.MVC/HtmlHelpers/NorthwindImageExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebUI.MVC.HtmlHelpers
+{
+    public static class NorthwindImageExtensions
+    {
+        public static HtmlString NorthwindImageLink(this IHtmlHelper htmlHelper, int imageId, string linkText)
+        {
+            var result = $"<a href=images/{imageId}>{linkText}</a>";
+            return new HtmlString(result);
+        }
+    }
+}
---
+++ src/WebUI.MVC/TagHelpers/NorthwindImageTagHelper.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace WebUI.MVC.TagHelpers
+{
+    [HtmlTargetElement("a", Attributes = "northwind-id")]
+    public class NorthwindImageTagHelper : TagHelper
+    {
+        // PascalCase gets translated into kebab-case.
+        public int NorthwindId { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.Attributes.SetAttribute("href", $"images/{NorthwindId}");
+        }
+    }
+}
